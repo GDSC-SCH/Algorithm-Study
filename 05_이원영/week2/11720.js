98619a562631a533bd6df2da5897b3386bfaa457
210-reverof <74983448+210-reverof@users.noreply.github.com>
2021-01-27 22:01:20 +0900

    Create 11720.js

---
+++ 05_이원영/week2/11720.js
@@ -0,0 +1,13 @@
+//7-2 11720
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let casesCount = Number(input[0]);
+let cases = input[1].split('');
+let sum = 0;
+
+for (let i = 0; i < casesCount; i++) {
+  sum = sum + Number(cases[i]);
+}
+
+console.log(sum);
