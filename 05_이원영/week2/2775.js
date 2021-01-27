7bb5aeb2a58ecdcf411e38e23056a43143e5edf3
210-reverof <74983448+210-reverof@users.noreply.github.com>
2021-01-27 22:08:40 +0900

    Create 2775.js

---
+++ 05_이원영/week2/2775.js
@@ -0,0 +1,27 @@
+//8-1 2775
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n').map(num => Number(num));
+ 
+const input = [2, 1, 3, 2, 3];
+const get = input.shift();
+ 
+for (let i = 0; i < get; i++) {
+const a = input.shift();
+const b = input.shift();
+const apartment = [];
+ 
+for (let i = 0; i <= a; i++) {
+apartment.push([1]);
+for (let j = 1; j < b; j++) {
+if (i === 0) {
+apartment[i].push(j + 1);
+} else {
+apartment[i].push(apartment[i][j - 1] + apartment[i - 1][j]);
+}
+}
+}
+ 
+const floor = a;
+const room = b - 1;
+console.log(apartment[floor][room]);
+}
