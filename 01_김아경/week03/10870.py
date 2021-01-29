dfc3cb50d506cb5f12f79442d0d61476dadaf4c9
EP000 <bzxz55@gmail.com>
2021-01-29 23:12:33 +0900

    Week02 step10

---
+++ 01_김아경/week03/10870.py
@@ -0,0 +1,14 @@
+n = int(input())
+a = 0
+b = 1
+fibonacci = 0
+
+if n != 1:
+    for i in range(n-1):
+        fibonacci = a + b
+        a = b
+        b = fibonacci
+        
+    print(fibonacci)
+else:
+    print(1)
---
+++ 01_김아경/week03/11729.py
@@ -0,0 +1,13 @@
+N = int(input())
+
+def hanoi(n, start, end):
+    mid = 6 - start - end
+    if n == 1:
+        return [[start, end]]
+    else:
+        return hanoi(n-1, start, mid) + [[start, end]] + hanoi(n-1, mid, end)
+    
+result = hanoi(N, 1, 3)
+print(len(result))
+for a, b in result:
+    print(a, b)
