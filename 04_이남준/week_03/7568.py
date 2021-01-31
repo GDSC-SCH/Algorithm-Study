0c08447eaabe57c81ffcec209c318c6df8e21632
ningpop <craft1933@naver.com>
2021-01-31 18:16:34 +0900

    [ningpop] : week 3 - no.11 풀이

---
+++ 04_이남준/week_03/7568.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+people = []
+for i in range(0, N):
+    x, y = map(int, input().split())
+    people.append((x, y))
+
+for i in people:
+    rank = 1
+    for j in people:
+        if i[0] < j[0] and i[1] < j[1]:
+            rank += 1
+        
+    print(rank, end=" ")
