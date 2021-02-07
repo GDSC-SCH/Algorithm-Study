4b3be3385677b2798ea02da15d88182d7df017c2
LeeInGyu <developlee20@gmail.com>
2021-02-07 21:40:03 +0900

    2021-02-07 week1 add remark

--- 06_이인규/week1/1110.py
+++ 06_이인규/week1/1110.py
@@ -1,8 +1,8 @@
 import sys
 
-a = int(sys.stdin.readline())
-b = a
-cnt = 0
+a = int(sys.stdin.readline()) # 입력
+b = a # 변화되는 값이 원래 값과 비교될때 사용
+cnt = 0 # 횟수 카운트
 
 while 1:
     c = (b//10 + b%10) % 10
--- 06_이인규/week1/2588.py
+++ 06_이인규/week1/2588.py
@@ -3,6 +3,7 @@ import sys
 a = sys.stdin.readline()
 b = sys.stdin.readline()
 
+# 문자열 형식으로 변수를 입력받아서 사용함
 print(int(a)*int(b[2]))
 print(int(a)*int(b[1]))
 print(int(a)*int(b[0]))
