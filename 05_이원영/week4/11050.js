c551311a6f991a2decf47e539ec4112dc28a4f4a
210-reverof <74983448+210-reverof@users.noreply.github.com>
2021-02-10 18:59:33 +0900

    w4

--- 05_이원영/week4/11050.js
+++ 05_이원영/week4/11050.js
@@ -3,15 +3,15 @@
 
 const N = 5;
 const K = 2;
-var arr = new Array(100);
+var arr = [[],[]];
 
 for (var i = 0; i <= N; i++) {
     for (var j = 0; j <= Math.min(K,i); j++) {
         if (i == j || j == 0)
-            arr[i, j] = 1;
+            arr[i][j] = 1;
         else
-            arr[i, j] = arr[i - 1, j - 1] + arr[i - 1, j];
+            arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
     }
 }
 
-console.log(arr[N, K]);
+console.log(arr[N][K]);
