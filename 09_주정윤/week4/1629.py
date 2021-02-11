b3328a0894a842036f910a34677c4d91c6fc1d28
JOLLA99 <yoon6148@daum.net>
2021-02-11 23:45:02 +0900

    4week_last

---
+++ 09_주정윤/week4/1629.py
@@ -0,0 +1,15 @@
+def gob(a, b):
+    if b == 1: 
+        return a % c
+    else:
+        temp = gob(a, b//2) 
+        if b % 2 == 0:
+            return temp * temp % c 
+        else:
+            return temp * temp * a % c
+
+
+a, b, c = map(int, input().split())
+
+result = gob(a, b)
+print(result)
