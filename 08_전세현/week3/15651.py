cb59e7cf477dbb702642028cfbd68ef7364723a2
jeonhl7579 <tpgus19@gmail.com>
2021-02-11 23:46:34 +0900

    create BOJ15651

---
+++ 08_전세현/week3/15651.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+N,M=map(int,input().split())
+
+num_list=[i+1 for i in range(N)]
+check_list=[False]*N
+
+arr=[]
+
+def dfs(cnt):
+    if cnt==M:
+        print(*arr)
+        return
+    for i in range(0,N):
+        arr.append(num_list[i])
+        dfs(cnt + 1)
+        arr.pop()
+
+dfs(0)
