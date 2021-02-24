2e4b83bc51cf3d2d9d1560cb60e24b927aafe281
LeeInGyu <developlee20@gmail.com>
2021-02-25 07:50:29 +0900

    2021-02-25

--- 06_이인규/week3/1463.py
+++ 06_이인규/week3/1463.py
@@ -1,15 +1,47 @@
-import math
+# 버리긴 아까운것...
+# import sys
 
-lst = []
-N =int(input())
+# lst = []
 
-x_range = int(math.log(N,3)+1)
-y_range = int(math.log(N,2)+1)
+# def make_one(num,cnt):
 
-for x in range(x_range):
-    for y in range(y_range):
-        toss = 3**x * 2**y
-        if toss <= N:
-            lst.append(N-toss+x+y)
+#     if num == 1:
+#         lst.append(cnt)
+#         return
 
-print(min(lst))
+#     if cnt == min(lst)-1:
+#         return
+
+#     if num % 3 == 0:
+#         make_one(num//3,cnt+1)
+    
+#     if num % 2 == 0:
+#         make_one(num//2,cnt+1)
+    
+#     if cnt < min(lst):
+#         make_one(num-1,cnt+1)
+
+# num = int(sys.stdin.readline())
+
+# lst.append(num//3+num%3)
+# make_one(num,0)
+# print(min(lst))
+
+import sys
+
+num = int(sys.stdin.readline())
+
+lst = [0,1,1,2,3]
+
+for i in range(6,num+1):
+    lst.append(0)
+
+    lst[i-1] = lst[i-2] + 1
+
+    if i % 3 == 0 and lst[i-1] > lst[i//3-1] + 1:
+        lst[i-1] = lst[i//3-1] + 1
+    
+    if i % 2 == 0 and lst[i-1] > lst[i//2-1] + 1:
+        lst[i-1] = lst[i//2-1] + 1
+
+print(lst[num-1])
--- 06_이인규/week3/14888.py
+++ 06_이인규/week3/14888.py
@@ -1,16 +1,32 @@
-# 미완
+import sys
 
-def all(sum,ind,plus,unplus,undiv,div,a):
-    if plus+unplus+undiv+div == a-1:
-        result.append([sum])
+lst = []
+res = []
+
+def calculator(num_index,result,p,m,q,d):
+
+    if p+m+d+q == 0:
+        res.append(result)
         return
-    all(sum+llist[ind],ind,plus+1,unplus,undiv,div,a)
-    all(sum-llist[ind],ind,plus,unplus+1,undiv,div,a)
-    all(sum*llist[ind],ind,plus,unplus,undiv+1,div,a)
-    all(sum/llist[ind],ind,plus,unplus,undiv,div+1,a)
 
-llist = []
-result = []
+    if p > 0:
+        calculator(num_index+1,result+lst[num_index],p-1,m,q,d)
+    if m > 0:
+        calculator(num_index+1,result-lst[num_index],p,m-1,q,d)
+    if q > 0:
+        calculator(num_index+1,result*lst[num_index],p,m,q-1,d)
+    if d > 0:
+        if result < 0:
+            result = abs(result)//lst[num_index]
+            calculator(num_index+1,-result,p,m,q,d-1)
+        else:
+            calculator(num_index+1,result//lst[num_index],p,m,q,d-1)
 
-a = int(input())
-llist = list(map(int,input().split()))
+int(sys.stdin.readline())
+lst = list(map(int,sys.stdin.readline().split()))
+p, m, q, d = map(int,sys.stdin.readline().split())
+
+calculator(1,lst[0],p,m,q,d)
+
+print(max(res))
+print(min(res))
