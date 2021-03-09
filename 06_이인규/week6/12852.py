4ebb7026df7a93e0e783fee0066a4a729a425a04
LeeInGyu <developlee20@gmail.com>
2021-03-09 11:41:14 +0900

    2021-03-09

--- 06_이인규/week6/12852.py
+++ 06_이인규/week6/12852.py
@@ -1,3 +1,5 @@
+# 보류
+
 import sys
 
 num = int(sys.stdin.readline())
@@ -16,10 +18,10 @@ for i in range(4,num+1):
         lst[i-1] = lst[i//3-1] + 1
     
     if i % 2 == 0 and lst[i-1] > lst[i//2-1] + 1:
-        gd = i//2
+        gd = i//3
         lst[i-1] = lst[i//2-1] + 1
 
     log += ' ' + str(gd)
 
 print(lst[num-1])
-print(log[:-1])
+print(log)
---
+++ 06_이인규/week6/13460.py
@@ -0,0 +1,8 @@
+# 펑
+
+x,y = map(int,input().split())
+lst = []
+
+for _ in range(x):
+    lst.append(input())
+
